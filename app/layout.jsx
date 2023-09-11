3a10982321bd42e96bc0fc150ac2ba4c01ad31b4
sameerse <khadkasameer23@gmail.com>
2023-09-11 15:15:08 +0545

    routing

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import NavBar from "./components/navbar";
-import Footer from "./components/footer";
-import Home from "./page";
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <NavBar />
-      <Home />
+      {/* <NavBar /> */}
       <body className={inter.className}>{children}</body>
-      <Footer />
+      {/* <Footer /> */}
     </html>
   );
 }
