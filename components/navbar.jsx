88dd9efd45e4482555dff563db72942f1880b4aa
sameerse <khadkasameer23@gmail.com>
2023-09-13 22:12:45 +0545

    css updates

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,19 +3,19 @@ import React from "react";
 export default function NavBar() {
   return (
     <div className="container mt-5 mb-5 bg-white flex justify-between text-primary ">
-      <div className="font-bold text-base">Dream Jobs</div>
-      <div className="flex gap-4 text-black text-sm t-w-[400px]">
-        <button>Home</button>
-        <button>Find Jobs</button>
-        <button>Find Candidates</button>
-        <button>Articles</button>
+      <div className="font-bold text-4xl">Dream Jobs</div>
+      <div className="flex gap-8 text-black text-lg t-w-[400px]">
+        <button className="hover:text-primary">Find Jobs</button>
+        <button className="hover:text-primary">Home</button>
+        <button className="hover:text-primary">Find Candidates</button>
+        <button className="hover:text-primary">Articles</button>
       </div>
-      <div className="flex gap-4">
-        <button className="text-primary" type="submit">
+      <div className="flex gap-6 text-lg">
+        <button className="text-primary  hover:text-teal-400" type="submit">
           Login
         </button>
         <button
-          className="text-white  bg-primary pt-2 pb-2 pl-4 pr-4 rounded-lg"
+          className="text-white  bg-primary pt-2 pb-2 pl-4 pr-4 rounded-lg hover:bg-teal-400"
           type="submit"
         >
           Register Now
