610c61133effc77a43b257fe0884def6b9d89fe5
sameerse <khadkasameer23@gmail.com>
2023-09-15 22:27:41 +0545

    landing page

--- app/(main)/(routes)/page.jsx
+++ app/(main)/(routes)/page.jsx
@@ -1,25 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export default function Home() {
   return (
     <div className="container">
-      <div className="h-96 flex flex-col justify-around">
-        <div className="text-7xl text-center">
+      <div className="h-[600px] flex flex-col justify-around">
+        <div className="text-9xl text-center">
           Get The <span className="text-primary">Right Job</span> <p />
           You Deserve
         </div>
         <div className="text-3xl text-center">
           786 jobs & 110 candidates are registeresd
         </div>
-        <div className="flex gap-2 justify-center">
+        <div className="flex justify-center rounded-xl">
           <input
-            className="border-secondary border-4 rounded-2xl p-3"
+            className="border-gray-200 border-4 rounded-s-2xl p-3 w-[450px] h-18"
             type="search"
             name="search"
             id="search"
           />
           <button
-            className="bg-primary text-white pt-3 pb-3 pl-6 pr-6 rounded-2xl"
+            className="bg-primary text-2xl text-white pt-3 pb-3 pl-10 pr-10 rounded-e-2xl"
             type="submit"
           >
             Search
--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
       <body className={roboto.className}>
         <NavBar />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
